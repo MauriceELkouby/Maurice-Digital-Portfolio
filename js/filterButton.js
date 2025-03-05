e26d690d30067801ed0f4c1e1e292aaabcaa69c9
MauriceELkouby <151778578+MauriceELkouby@users.noreply.github.com>
2025-03-05 00:31:48 -0500

    Adding filters, boggle solver and new game

---
+++ js/filterButton.js
@@ -0,0 +1,16 @@
+    const filterButtons = document.querySelectorAll('.filter-button');
+    const projectCards = document.querySelectorAll('.project-card');
+
+    filterButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const type = button.getAttribute('data-type');
+
+            projectCards.forEach(card => {
+                if (type === 'all' || card.getAttribute('data-type') === type) {
+                    card.style.display = 'block';
+                } else {
+                    card.style.display = 'none';
+                }
+            });
+        });
+    });
