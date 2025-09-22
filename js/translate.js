78b603c2e2204a6c6a2c38ff7dd3b687e6612dd9
MauriceELkouby <151778578+MauriceELkouby@users.noreply.github.com>
2025-09-22 11:32:54 -0400

    Adding translator

---
+++ js/translate.js
@@ -0,0 +1,38 @@
+// 1) Load Google’s script
+  (function () {
+    var s = document.createElement('script');
+    s.src = 'https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    document.head.appendChild(s);
+  })();
+
+  // 2) Init into our hidden container (required)
+  function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+      pageLanguage: 'en',
+      includedLanguages: 'en,fr',
+      autoDisplay: false
+    }, 'google_translate_container');
+  }
+
+  // 3) Robust cookie setter (current host + bare domain)
+  function setGoogTrans(val) {
+    var d = new Date(); d.setTime(d.getTime() + 365*24*60*60*1000);
+    var exp = ';expires=' + d.toUTCString() + ';path=/';
+    document.cookie = 'googtrans=' + val + exp; // current host
+    try {
+      var baseDomain = location.hostname.replace(/^www\./, '');
+      document.cookie = 'googtrans=' + val + exp + ';domain=' + baseDomain;
+    } catch (e) {}
+  }
+
+  // 4) Click handler: set cookie then reload (don’t touch location.hash)
+  document.addEventListener('click', function (e) {
+    const btn = e.target.closest('[data-lang]');
+    if (!btn) return;
+    const to = btn.getAttribute('data-lang'); // 'en' or 'fr'
+    setGoogTrans('/en/' + to);
+    location.reload(); // cookie makes Google auto-translate on load
+  });
+
+  // 5) Optional: if a cookie already exists, nothing more to do.
+  // Google will read it on page load and translate automatically.
