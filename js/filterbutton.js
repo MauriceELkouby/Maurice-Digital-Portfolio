6b24126b0276a0c52799638b90951a06cc52e545
MauriceELkouby <151778578+MauriceELkouby@users.noreply.github.com>
2025-09-24 12:25:25 -0400

    adding button events to analytics

--- js/filterbutton.js
+++ js/filterbutton.js
@@ -12,5 +12,9 @@
                     card.style.display = 'none';
                 }
             });
+            gtag('event', 'filter_selected', {
+            'filter_type': type
+            });
+            
         });
     });
